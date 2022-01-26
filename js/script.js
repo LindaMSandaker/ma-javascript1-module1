bc38c230feb12b87f244c0fad81e146e669715d7
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-26 21:49:53 +0100

    Questions 1-5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,66 @@
+// Question 1
+
+const cat = {
+    complain: function (says) {
+        console.log('Meow!');
+    }
+};
+
+cat.complain();
+
+// ************
+
+
+// Question 2
+
+const heading = document.querySelector("h3");
+console.log(heading);
+
+heading.innerHTML = "Updated heading";
+
+// ************
+
+
+// Question 3
+
+heading.style.fontSize = "2em";
+console.log(heading);
+
+// ************
+
+
+// Question 4
+
+heading.classList.add("title");
+console.log(heading);
+
+// ************
+
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+console.log(paragraphs);
+
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+    console.log(paragraphs[i]);
+}
+
+// ************
+
+
+// Question 6
+
+// ************
+
+
+// Question 7
+
+// ************
+
+
+// Question 8
 const cats = [
     {
         name: "Blob",
